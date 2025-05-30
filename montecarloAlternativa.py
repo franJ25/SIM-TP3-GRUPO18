07d77c219471b6a10b562f3dd353d4dbfdf2e111
Francisco Jalile <francisco2004jalile@gmail.com>
2025-05-30 09:48:31 -0300

    versión alternativa

---
+++ montecarloAlternativa.py
@@ -0,0 +1,68 @@
+import random
+import interfazAlternativa
+
+
+def calcular_ausentismo(probabilidades_acumuladas, num_obreros_aus):
+    aleatorio = random.random()
+    for indice, prob_acumulada in enumerate(probabilidades_acumuladas):
+        if aleatorio < prob_acumulada:
+            return num_obreros_aus[indice]
+    return num_obreros_aus[-1]
+
+
+def calcular_beneficio_diario(conductores_totales, presentes, ingreso_diario, costo_operativo, salario):
+    ingreso_total = 0
+    costo_salario = conductores_totales * salario
+    costo_total = costo_operativo + costo_salario
+
+    if presentes >= 20:
+        ingreso_total = ingreso_diario
+    return ingreso_total - costo_total, costo_salario, costo_total, ingreso_total
+
+
+def calcular_distribucion(datos_ausentismo):
+    distribucion = []
+    total = sum(datos_ausentismo)
+    for dato in datos_ausentismo:
+        probabilidad = dato / total
+        distribucion.append(probabilidad)
+    return distribucion
+
+
+def calcular_acumulada(distribucion):
+    probabilidades_acumuladas = []
+    for i in range(len(distribucion)):
+        probabilidades_acumuladas.append(sum(distribucion[:i + 1]))
+    return probabilidades_acumuladas
+
+
+def simular(conductores_totales, ingreso_diario, costo_operativo, salario, datos_ausentismo, n, i, j, tar):
+    distribucion = calcular_distribucion(datos_ausentismo)
+    probabilidades_acumuladas = calcular_acumulada(distribucion)
+    beneficio_acumulado = 0
+    ultima_fila = None
+
+    for dia in range(1, n + 1):
+        ausentes = calcular_ausentismo(probabilidades_acumuladas, [0, 1, 2, 3, 4, 5])
+        presentes = conductores_totales - ausentes
+        beneficio, costo_salario, costo_total, ingreso_total = calcular_beneficio_diario(conductores_totales, presentes,
+                                                                                         ingreso_diario,
+                                                                                         costo_operativo, salario)
+        beneficio_acumulado += beneficio
+        fila = {
+                'Día': dia,
+                'Ausentes': ausentes,
+                'Presentes': presentes,
+                'Ingreso': ingreso_total,
+                'Costo_Operativo': costo_operativo,
+                'Costo_Salario': costo_salario,
+                'Costo_Total': costo_total,
+                'Beneficio_Diario': beneficio,
+                'Beneficio_Acumulado': beneficio_acumulado
+            }
+        if j <= dia <= min(i + j - 1, n):
+            interfazAlternativa.imprimir_fila(fila, tar)
+        if dia == n:
+            ultima_fila = fila
+    return ultima_fila
+
