4d4a19b171ddaf3a77bfc98349025bebeec995ec
Francisco Jalile <francisco2004jalile@gmail.com>
2025-06-24 09:38:48 -0300

    nuevo

--- montecarloAlternativa.py
+++ montecarloAlternativa.py
@@ -10,14 +10,11 @@ def calcular_ausentismo(probabilidades_acumuladas, num_obreros_aus):
     return num_obreros_aus[-1]
 
 
-def calcular_beneficio_diario(conductores_totales, presentes, ingreso_diario, costo_operativo, salario):
+def calcular_beneficio_diario(presentes, ingreso_diario, costo_total):
     ingreso_total = 0
-    costo_salario = conductores_totales * salario
-    costo_total = costo_operativo + costo_salario
-
     if presentes >= 20:
         ingreso_total = ingreso_diario
-    return ingreso_total - costo_total, costo_salario, costo_total, ingreso_total
+    return ingreso_total - costo_total
 
 
 def calcular_distribucion(datos_ausentismo):
@@ -36,21 +33,29 @@ def calcular_acumulada(distribucion):
     return probabilidades_acumuladas
 
 
-def simular(conductores_totales, ingreso_diario, costo_operativo, salario, datos_ausentismo, n, i, j, tar):
+def simular(conductores_totales, ingreso_diario, costo_operativo, salario, datos_ausentismo, n, i, j):
     distribucion = calcular_distribucion(datos_ausentismo)
     probabilidades_acumuladas = calcular_acumulada(distribucion)
     beneficio_acumulado = 0
     ultima_fila = None
 
     for dia in range(1, n + 1):
+        # Obtener el RND para el ausentismo
+        rnd_ausentismo = random.random()
         ausentes = calcular_ausentismo(probabilidades_acumuladas, [0, 1, 2, 3, 4, 5])
         presentes = conductores_totales - ausentes
-        beneficio, costo_salario, costo_total, ingreso_total = calcular_beneficio_diario(conductores_totales, presentes,
-                                                                                         ingreso_diario,
-                                                                                         costo_operativo, salario)
+
+        # Calcular columnas
+        ingreso_total = 0
+        costo_salario = conductores_totales * salario
+        costo_total = costo_operativo + costo_salario
+
+        # Calcular beneficio diario
+        beneficio = calcular_beneficio_diario(presentes, ingreso_diario, costo_total)
         beneficio_acumulado += beneficio
         fila = {
                 'Día': dia,
+                'RND Ausentismo': rnd_ausentismo,
                 'Ausentes': ausentes,
                 'Presentes': presentes,
                 'Ingreso': ingreso_total,
@@ -61,8 +66,12 @@ def simular(conductores_totales, ingreso_diario, costo_operativo, salario, datos
                 'Beneficio_Acumulado': beneficio_acumulado
             }
         if j <= dia <= min(i + j - 1, n):
-            interfazAlternativa.imprimir_fila(fila, tar)
+            interfazAlternativa.imprimir_fila(fila)
         if dia == n:
             ultima_fila = fila
-    return ultima_fila
+    return {
+        'ultima_fila': ultima_fila,
+        'distribucion': distribucion,
+        'probabilidades_acumuladas': probabilidades_acumuladas
+    }
 
